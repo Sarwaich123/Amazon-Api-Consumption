1f87bb627320fcb6cfdca3b2f83ad68f369ba086
Sarwaich Raza <sarwaich.raza@sapienssquare.com>
2024-02-02 22:59:20 -0700

    Initial commit

---
+++ Methods.cs
@@ -0,0 +1,104 @@
+﻿using Nager.AmazonProductAdvertising;
+using Nager.AmazonProductAdvertising.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace ConsumeApiSolution
+{
+   public class Methods
+    {
+       
+  
+        public async Task<List<string>> Searchitems(string keyword,string secretkey, string accesskey, string partnertag)
+        {
+            var authentication = new AmazonAuthentication();
+            authentication.AccessKey = "Access-Key";
+            authentication.SecretKey = "Secret-Key";
+            string partnertag1 = "partnertag"; 
+           var client = new AmazonProductAdvertisingClient(authentication, AmazonEndpoint.US, partnertag1);
+            var searchRequest = new SearchRequest
+            {
+                Keywords = keyword,
+                ItemPage = 1,
+                Resources = new[]
+             {
+             //https://webservices.amazon.com/paapi5/documentation/search-items.html#resources-parameter
+            "Images.Primary.Large",
+            "ItemInfo.Title",
+            "ItemInfo.Features"
+             }
+            };
+            var result = await client.SearchItemsAsync(searchRequest);
+            int abc = result.SearchResult.TotalResultCount;
+            Console.WriteLine("Total number of result items are "+abc);
+            var names = new List<string>();
+            for (int w = 0; w < 10; w++)
+            {
+                names.Add(result.SearchResult.Items[w].ASIN);
+            }
+            int l = abc / 10;
+            if (l >= 10)
+                l = 10;
+            if (abc > 10)
+            {
+
+                for (int i = 2; i <= l; i++)
+                {
+                    var searchRequest1 = new SearchRequest
+                    {
+                        Keywords = keyword,
+                        ItemPage = i,
+                        Resources = new[]
+                    {
+                     //You can found all available Resources in the documentation
+                    //https://webservices.amazon.com/paapi5/documentation/search-items.html#resources-parameter
+                    "Images.Primary.Large",
+                    "ItemInfo.Title",
+                    "ItemInfo.Features"
+                    }
+                    };
+                    var result1 = await client.SearchItemsAsync(searchRequest1);
+                    for (int w = 0; w < 10; w++)
+                    {
+                        names.Add(result1.SearchResult.Items[w].ASIN);
+                       // r++;
+                    }
+
+                }
+            }
+
+            return names;
+        }
+
+        public async Task<string> Getitems(string asin, string secretkey, string accesskey, string partnertag)
+        {
+            var authentication = new AmazonAuthentication();
+            authentication.AccessKey = accesskey;
+            authentication.SecretKey = secretkey;
+            var client = new AmazonProductAdvertisingClient(authentication, AmazonEndpoint.US, partnertag);
+            var result = await client.GetItemsAsync(asin);
+            // var result = await client.SearchItemsAsync(as);
+            return result.ToString();
+        }
+        public async Task<string> GetVariations(string asin, string secretkey, string accesskey, string partnertag)
+        {
+            var authentication = new AmazonAuthentication();
+            authentication.AccessKey = accesskey;
+            authentication.SecretKey = secretkey;
+            var client = new AmazonProductAdvertisingClient(authentication, AmazonEndpoint.US, partnertag);
+            var result = await client.GetVariationsAsync(asin);
+            // var result = await client.SearchItemsAsync(as);
+            return result.ToString();
+        }
+
+        public async Task<string> Browsernodes(string[] brow, string secretkey, string accesskey, string partnertag)
+        {
+            var authentication = new AmazonAuthentication();
+            authentication.AccessKey = accesskey;
+            authentication.SecretKey = secretkey;
+            var client = new AmazonProductAdvertisingClient(authentication, AmazonEndpoint.US, partnertag);
+            var result = await client.GetBrowseNodesAsync(brow);
+            return result.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using Nager.AmazonProductAdvertising;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsumeApiSolution
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Amazon Advertising Apis Services");
+            Console.WriteLine("Enter Secret Key");
+            string secretkey = Console.ReadLine();
+            Console.WriteLine("Enter access Key");
+            string accessKey = Console.ReadLine();
+            Console.WriteLine("Enter Partner-tag Key");
+            string partnertag = Console.ReadLine();
+            int selection = display();
+            Methods mm = new Methods();
+            if (selection == 1)
+            {
+                Console.WriteLine("Enter Keyword to Search");
+                string keyword = Console.ReadLine();
+                var result = new List<string>(100);
+                result = await mm.Searchitems(keyword, secretkey, accessKey, partnertag);
+                Console.WriteLine(result);
+            }
+            else if (selection==2)
+            {//Getitems....
+                Console.WriteLine("Enter ASIN to Search");
+                string asin = Console.ReadLine();
+                string result = await mm.Getitems(asin, secretkey, accessKey, partnertag);
+                Console.WriteLine(result);
+
+            }
+            else if (selection == 3)
+            {
+                Console.WriteLine("Enter ASIN to Search");
+                string asin = Console.ReadLine();
+                string result = await mm.GetVariations(asin, secretkey, accessKey, partnertag);
+                Console.WriteLine(result);
+            }
+            else if (selection == 4)
+            {
+                string[] nodess= GetNodes();
+                string result = await mm.Browsernodes(nodess, secretkey, accessKey, partnertag);
+                Console.WriteLine(result);
+            }
+            else
+            { 
+                Console.WriteLine("You have selected wrong number");
+                Console.WriteLine("Do you want to try again press Y to try again");
+                string value=Console.ReadLine();
+                if(value=="Y")
+                {
+                    await Main(args);
+                }
+            }
+
+        }
+
+        static public int display()
+        {
+            
+            Console.WriteLine("Select service you want to avail?");
+            Console.WriteLine("1:Searchitems");
+            Console.WriteLine("2:Getitems");
+            Console.WriteLine("3:GetVariations");
+            Console.WriteLine("4:GetNodes");
+            string response = Console.ReadLine();
+            int final_response = Convert.ToInt32(response);
+            return final_response;
+        }
+
+
+        public static string[] GetNodes()
+        {
+
+            var names = new List<string>();
+          //  int index = 0;
+            string decision = "W";
+            do
+            {
+             //   Console.WriteLine(index);
+                Console.WriteLine("Enter Node value");
+                string value = Console.ReadLine();
+                names.Add(value);
+              //  index++;
+                Console.WriteLine("Do you want to add more Node? if Yes press any key..if No press N");
+                decision = Console.ReadLine();
+
+            } while (decision != "N");
+            //  Console.WriteLine(nodes);
+            //  return nodes;
+            var nameArray = names.ToArray();
+            return nameArray;
+        }
+
+
+
+
+    }
+}
